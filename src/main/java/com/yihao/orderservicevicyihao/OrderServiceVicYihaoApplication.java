7c0f56c6ba87cf89aea45f7663ce6db5c7d6bd42
aiaiaiyihao <aiaiaiyihao@gmail.com>
2026-03-16 13:11:01 -0700

    initialize order service

---
+++ src/main/java/com/yihao/orderservicevicyihao/OrderServiceVicYihaoApplication.java
@@ -0,0 +1,13 @@
+package com.yihao.orderservicevicyihao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceVicYihaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceVicYihaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yihao/orderservicevicyihao/OrderServiceVicYihaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yihao.orderservicevicyihao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceVicYihaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
